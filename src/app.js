fce259aa796c913b5103993a94dfc86333264f71
Abhinay Parvathaneni <pabhinay43@gmail.com>
2022-09-06 05:59:58 +0000

    emit events

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const server = require('http').createServer();
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*'
+  }
+});
 const { triggerEventInLoop } = require('./mock/index')
 
 io.on('connection', client => {
@@ -7,11 +11,12 @@ io.on('connection', client => {
   client.on('event', data => { /* … */ });
   client.on('disconnect', () => { /* … */ });
   client.onAny((event, args) => {
+    client.emit(event, args)
     console.log(`got ${event}`);
     console.log(JSON.stringify(args));
   });
 });
-server.listen(3000);
+server.listen(3000, '0.0.0.0');
 
 
 // trigger mock event
