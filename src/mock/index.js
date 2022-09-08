7bd84a4b42f3467b788aa248be464a5b9f168faf
Abhinay Parvathaneni <pabhinay43@gmail.com>
2022-09-08 05:05:20 +0000

    update trigger port

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,7 +1,7 @@
 const { Manager } = require("socket.io-client");
 const { eventInfo } = require('./data/index')
 
-const SERVER_URL = 'ws://localhost:3000'
+const SERVER_URL = 'ws://localhost:80'
 
 const manager = new Manager(SERVER_URL, {
   reconnectionDelayMax: 10000,
